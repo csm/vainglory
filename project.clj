ffa3e0c0528f07baf3b8d3e8de72f28025c61477
Casey Marshall <csm@noonhome.com>
2019-08-29 21:50:52 -0700

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.csm/vainglory "0.1.1"
+(defproject com.github.csm/vainglory "0.1.2-SNAPSHOT"
   :description "Data-driven clients for Swagger APIs"
   :url "https://github.com/csm/vainglory"
   :license {:name "MIT"
