3f66d0d56c7fb94bcea067fb7187c030d6b04436
Casey Marshall <csm@noonhome.com>
2019-08-29 21:33:47 -0700

    Remove unused dependencies.

--- project.clj
+++ project.clj
@@ -5,9 +5,6 @@
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "0.4.500"]
-                 [org.clojure/core.cache "0.7.2"]
-                 [org.clojure/core.memoize "0.7.2"]
-                 [com.cognitect/http-client "0.1.99"]
                  [com.cognitect/anomalies "0.1.12"]
                  [metosin/jsonista "0.2.4"]
                  [io.forward/yaml "1.0.9"]
